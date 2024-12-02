db727665abb0896eaae5f6d988b6f2fd5a599d98
Alfie Atkinson <alf.atkinson13@gmail.com>
2024-12-02 13:34:28 +0000

    Add tests for Application::loginUser and Application::logoutUser

--- tests/ApplicationTest.cpp
+++ tests/ApplicationTest.cpp
@@ -2,6 +2,8 @@
 #include <catch2/catch.hpp>
 #include "Application.hpp"
 
+// Tests for Application::start
+
 TEST_CASE("start sets is_running to true") {
     Application app;
     REQUIRE_FALSE(app.is_running);
@@ -30,5 +32,24 @@ TEST_CASE("start initialises to login menu") {
 TEST_CASE("start initialises user as null") {
     Application app;
     app.start();
+    REQUIRE(app.getCurrentUser() == nullptr);
+}
+
+// Tests for Application::loginUser and Application::logoutUser
+
+TEST_CASE("loginUser sets current_user to the correct user") {
+    Application app;
+    User test_user(1, "test", "password123")
+    app.start();
+    app.loginUser(test_user);
+    REQUIRE(app.getCurrentUser() == &test_user);
+}
+
+TEST_CASE("logoutUser sets current_user to null") {
+    Application app;
+    User test_user(1, "test", "password123")
+    app.start();
+    app.loginUser(test_user);
+    app.logoutUser();
     REQUIRE(app.getCurrentUser() == nullptr);
 }
