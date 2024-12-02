3f411f0e0625c7412a893c12bb874f2bbda4f22b
Alfie Atkinson <alf.atkinson13@gmail.com>
2024-12-02 14:50:32 +0000

    Add tests for Book class constructor and its properties

--- tests/BookTest.cpp
+++ tests/BookTest.cpp
@@ -1,2 +1,27 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include "Book.hpp"
+
+TEST_CASE("Book Constructor initializes correctly") {
+    Book book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", 1925, true);
+
+    SECTION("Title is correct") {
+        REQUIRE(book.getTitle() == "The Great Gatsby");
+    }
+    
+    SECTION("Author is correct") {
+        REQUIRE(book.getAuthor() == "F. Scott Fitzgerald");
+    }
+
+    SECTION("ISBN is correct") {
+        REQUIRE(book.getISBN() == "9780743273565");
+    }
+
+    SECTION("Year Published is correct") {
+        REQUIRE(book.getYearPublished() == 1925);
+    }
+
+    SECTION("Availability is correct") {
+        REQUIRE(book.isAvailable() == true);
+    }
+}
