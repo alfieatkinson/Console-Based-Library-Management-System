8559c409625b454b1c447ee61b9a6164011e4e9f
Alfie Atkinson <alf.atkinson13@gmail.com>
2024-12-03 14:22:08 +0000

    Add unit tests for User class constructor and validation of attributes

--- tests/UserTest.cpp
+++ tests/UserTest.cpp
@@ -1,2 +1,35 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include "User.hpp"
+
+TEST_CASE("User Constructor initialises correctly") {
+    User user(1, "John", "Doe", "johndoe@email.com", "01234567890", "john_doe", "password123");
+
+    SECTION("User ID is correct") {
+        REQUIRE(user.getUserID() == 1);
+    }
+
+    SECTION("Forename is correct") {
+        REQUIRE(user.getForename() == "John");
+    }
+
+    SECTION("Surname is correct") {
+        REQUIRE(user.getSurname() == "Doe");
+    }
+
+    SECTION("Email is correct") {
+        REQUIRE(user.getEmail() == "johndoe@email.com");
+    }
+
+    SECTION("Phone number is correct") {
+        REQUIRE(user.getPhoneNumber() == "01234567890");
+    }
+
+    SECTION("Username is correct") {
+        REQUIRE(user.getUsername() == "john_doe");
+    }
+
+    SECTION("Password is correct") {
+        REQUIRE(user.getPassword() == "password123");
+    }
+}
