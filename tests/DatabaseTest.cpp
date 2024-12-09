f58e748fc30785d954c3a723adc95d87fecd58ce
alfieatkinson <alf.atkinson13@gmail.com>
2024-12-09 10:57:18 +0000

    Add unit tests for Database class initialization

--- tests/DatabaseTest.cpp
+++ tests/DatabaseTest.cpp
@@ -1,2 +1,34 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include "Database.hpp"
+#include "Book.hpp"
+#include "User.hpp"
+#include "Transaction.hpp"
+
+TEST_CASE("Database constructor initialises correctly") {
+    Database db;
+
+    SECTION("Books is empty") {
+        REQUIRE(db.getBooks().empty());
+    }
+
+    SECTION("Book ID counter is 0") {
+        REQUIRE(db.getBookIDCounter() == 0);
+    }
+
+    SECTION("Users is empty") {
+        REQUIRE(db.getUsers().empty());
+    }
+
+    SECTION("User ID counter is 0") {
+        REQUIRE(db.getUserIDCounter() == 0);
+    }
+
+    SECTION("Transactions is empty") {
+        REQUIRE(db.getTransactions().empty());
+    }
+
+    SECTION("Transaction ID counter is 0") {
+        REQUIRE(db.getTransactionIDCounter() == 0);
+    }
+}
