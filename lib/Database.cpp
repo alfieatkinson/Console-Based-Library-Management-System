4666219567145411bcc7c89a4ce49dab45484edf
alfieatkinson <alf.atkinson13@gmail.com>
2024-12-09 12:54:35 +0000

    Add create operations for books, users, and transactions in Database class

--- lib/Database.cpp
+++ lib/Database.cpp
@@ -17,3 +17,16 @@ int Database::getTransactionIDCounter() const { return transaction_id_counter; }
 void Database::setBookIDCounter(int new_counter) { book_id_counter = new_counter; }
 void Database::setUserIDCounter(int new_counter) { user_id_counter = new_counter; }
 void Database::setTransactionIDCounter(int new_counter) { transaction_id_counter = new_counter; }
+
+// Create operations
+void Database::createBook(const std::string& title, const std::string& author, const std::string& isbn, int year) {
+    books.push_back(Book(++book_id_counter, title, author, isbn, year, true));
+}
+
+void Database::createUser(const std::string& username, const std::string& forename, const std::string& surname, const std::string& email, const std::string& phone, const std::string& password) {
+    users.push_back(User(++user_id_counter, username, forename, surname, email, phone, password));
+}
+
+void Database::createTransaction(const std::string& type, Book& book, User& user) {
+    transactions.push_back(Transaction(++transaction_id_counter, type, &book, &user));
+}
