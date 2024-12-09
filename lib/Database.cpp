4b094cc0f96e0f50ca4bd09e94b732ed4d2038c1
alfieatkinson <alf.atkinson13@gmail.com>
2024-12-09 12:46:37 +0000

    Implement getters in Database class for books, users, and transactions

---
+++ lib/Database.cpp
@@ -0,0 +1,14 @@
+#include "Database.hpp"
+#include "Book.hpp"
+#include "User.hpp"
+#include "Transaction.hpp"
+#include <algorithm>
+#include <stdexcept>
+
+// Getters
+const std::vector<Book>& Database::getBooks() const { return books; }
+const std::vector<User>& Database::getUsers() const { return users; }
+const std::vector<Transaction>& Database::getTransactions() const { return transactions; }
+int Database::getBookIDCounter() const { return book_id_counter; }
+int Database::getUserIDCounter() const { return user_id_counter; }
+int Database::getTransactionIDCounter() const { return transaction_id_counter; }
