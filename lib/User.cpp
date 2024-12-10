9154440eb836ffe5fdc3d70699f9674e7374c473
alfieatkinson <alf.atkinson13@gmail.com>
2024-12-10 12:23:28 +0000

    Refactor email validation regex in User class constructor for improved readability

--- lib/User.cpp
+++ lib/User.cpp
@@ -12,7 +12,7 @@ User::User(int user_id, const std::string& username, const std::string& forename
     }
 
     // Validate email format
-    std::regex email_regex(R"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])");
+    std::regex email_regex("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
     if (!std::regex_match(email, email_regex)) {
         throw std::invalid_argument("Invalid email format.");
     }
