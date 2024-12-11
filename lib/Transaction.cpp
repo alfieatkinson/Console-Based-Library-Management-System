898139b85da736c228e03034dc81a8e32ccc8526
alfieatkinson <alf.atkinson13@gmail.com>
2024-12-11 10:00:29 +0000

    Refactor Transaction class to use shared pointers for Book and User to enhance memory management

--- lib/Transaction.cpp
+++ lib/Transaction.cpp
@@ -4,28 +4,23 @@
 #include <ctime>
 
 // Constructor
-Transaction::Transaction(int transaction_id, const std::string& type, Book* book, User* user)
+Transaction::Transaction(int transaction_id, const std::string& type, std::shared_ptr<Book> book, std::shared_ptr<User> user)
     : transaction_id(transaction_id), type(type), status("open"), book(book), user(user) {
-    if (type != "borrow" && type != "return") {
-        throw std::invalid_argument("Invalid transaction type");
-    }
+    if (type != "borrow" && type != "return") throw std::invalid_argument("Invalid transaction type");
     datetime = ""; // Initially empty until executed
 }
 
-// Destructor
-Transaction::~Transaction() {
-    // No dynamic memory allocation to clean up
-}
-
 // Overloaded operator
-bool Transaction::operator==(const Transaction& other) const { return transaction_id == other.transaction_id; }
+bool Transaction::operator==(const Transaction& other) const {
+    return transaction_id == other.transaction_id;
+}
 
 // Getters
 int Transaction::getID() const { return transaction_id; }
 std::string Transaction::getType() const { return type; }
 std::string Transaction::getStatus() const { return status; }
-Book* Transaction::getBook() const { return book; }
-User* Transaction::getUser() const { return user; }
+std::shared_ptr<Book> Transaction::getBook() const { return book; }
+std::shared_ptr<User> Transaction::getUser() const { return user; }
 std::string Transaction::getDatetime() const { return datetime; }
 
 // Setters
@@ -39,17 +34,11 @@ void Transaction::execute() {
         datetime = ctime(&now);
 
         if (type == "borrow") {
-            if (user->borrowBook(book)) {
-                status = "completed";
-            } else {
-                throw std::invalid_argument("Book is not available to borrow.");
-            }
+            if (user->borrowBook(book)) status = "completed";
+            else throw std::invalid_argument("Book is not available to borrow.");
         } else if (type == "return") {
-            if (user->returnBook(book)) {
-                status = "completed";
-            } else {
-                throw std::invalid_argument("User has not borrowed this book.");
-            }
+            if (user->returnBook(book)) status = "completed";
+            else throw std::invalid_argument("User has not borrowed this book.");
         }
     } else {
         throw std::logic_error("Transaction is not open.");
