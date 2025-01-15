9ecdfa5e0c0eed51e7ab0c0269338f612a0befa0
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-15 14:52:04 +0000

    Add CRUD operations and database persistence methods to Library class

--- include/Library.hpp
+++ include/Library.hpp
@@ -28,11 +28,31 @@ public:
     void borrowBook(int book_id, int user_id);
     void returnBook(int book_id, int user_id);
 
-    // Methods for searching books
+    // CRUD Operations
+    void createBook(const std::vector<std::string>& book_info);
+    void createUser(const std::vector<std::string>& user_info);
+
+    void readBook(int id);
+    void readUser(int id);
+    void readTransaction(int id);
+
+    void updateBook(int id, const std::string& field, const std::string& value);
+    void updateUser(int id, const std::string& field, const std::string& value);
+    void updateTransaction(int id, const std::string& field, const std::string& value);
+
+    void deleteBook(int id);
+    void deleteUser(int id);
+    void deleteTransaction(int id);
+
+    // Methods for searching
     std::vector<std::shared_ptr<Book>> queryBooks(const std::string& search_term);
     std::vector<std::shared_ptr<User>> queryUsers(const std::string& search_term);
     std::vector<std::shared_ptr<Transaction>> queryTransactionsByBookID(int id);
     std::vector<std::shared_ptr<Transaction>> queryTransactionsByUserID(int id);
+
+    // Database persistence methods
+    void saveDatabase() const;
+    void loadDatabase() const;
 };
 
 #endif // LIBRARY_HPP
