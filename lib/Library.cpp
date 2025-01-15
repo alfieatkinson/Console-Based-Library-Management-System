782a9d2b033a3b24ecb6954c405c31b283e0687b
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-15 16:38:06 +0000

    Add createBook and createUser methods to LibraryManager for handling book and user creation

--- lib/Library.cpp
+++ lib/Library.cpp
@@ -62,3 +62,18 @@ void LibraryManager::returnBook(int book_id, int user_id) {
         throw std::invalid_argument("User has not borrowed this book.");
     }
 }
+
+// CRUD Operations
+void LibraryManager::createBook(const std::vector<std::string>& book_info) {
+    if (book_info.size() != 4) throw std::invalid_argument("Invalid number of arguments for creating a book.");
+
+    // Create a new book
+    db.createBook(book_info[0], book_info[1], book_info[2], std::stoi(book_info[3]));
+}
+
+void LibraryManager::createUser(const std::vector<std::string>& user_info) {
+    if (user_info.size() != 6) throw std::invalid_argument("Invalid number of arguments for creating a user.");
+
+    // Create a new user
+    db.createUser(user_info[0], user_info[1], user_info[2], user_info[3], user_info[4], user_info[5]);
+}
