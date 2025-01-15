01a9bbbcdd24d76de615d2134f24b2324edcdd49
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-15 16:34:56 +0000

    Add getters and setters for Database in LibraryManager class

--- lib/Library.cpp
+++ lib/Library.cpp
@@ -15,3 +15,14 @@ LibraryManager::~LibraryManager() {
     // Save the database on destruction
     saveDatabase();
 }
+
+// Getters
+const Database& LibraryManager::getDatabase() const {
+    return db;
+}
+
+// Setters
+void LibraryManager::setDatabase(const Database& new_db) {
+    db = new_db;
+}
+
