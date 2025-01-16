345f63e4c45473009370fc585535d218b272c91c
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-16 11:04:19 +0000

    Add tests for Menu class constructor and getters/setters

--- tests/MenuTest.cpp
+++ tests/MenuTest.cpp
@@ -11,3 +11,23 @@ std::string captureConsoleOutput(std::function<void()> func) {
     std::cout.rdbuf(oldCoutBuffer); // Reset cout
     return output.str();
 }
+
+TEST_CASE("Menu Constructor Initialises Correctly") {
+    Menu menu("Main Menu");
+
+    REQUIRE(menu.getName() == "Main Menu");
+    REQUIRE(menu.getOptions().empty());
+}
+
+TEST_CASE("Menu Getters and Setters") {
+    Menu menu("Main Menu");
+
+    SECTION("GetName returns correct name") {
+        REQUIRE(menu.getName() == "Main Menu");
+    }
+
+    SECTION("SetName updates the menu name") {
+        menu.setName("New Menu");
+        REQUIRE(menu.getName() == "New Menu");
+    }
+}
