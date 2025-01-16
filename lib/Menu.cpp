1bb012c8083ea1fd817a15e0a50594c9d9697cbe
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-16 11:11:22 +0000

    Add getters and setters for Menu class attributes

--- lib/Menu.cpp
+++ lib/Menu.cpp
@@ -6,3 +6,17 @@ Menu::Menu(const std::string& name) : name(name) {}
 // Destructor
 Menu::~Menu() {}
 
+// Getters
+std::string Menu::getName() const {
+    return name;
+}
+
+std::map<std::string, std::function<void()>> Menu::getOptions() const {
+    return options;
+}
+
+// Setters
+void Menu::setName(const std::string& name) {
+    this->name = name;
+}
+
