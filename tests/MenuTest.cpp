2c6c228fea830e24ce2d6181666a7d4c3c93941d
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-16 11:03:52 +0000

    Add console output capture helper to MenuTest

--- tests/MenuTest.cpp
+++ tests/MenuTest.cpp
@@ -1,2 +1,13 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <sstream>
+#include <iostream>
+
+// Helper for redirecting console output
+std::string captureConsoleOutput(std::function<void()> func) {
+    std::ostringstream output;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
+    func();
+    std::cout.rdbuf(oldCoutBuffer); // Reset cout
+    return output.str();
+}
