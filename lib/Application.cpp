06384cdb344824eff000a967a043818ca9ac3b81
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-21 15:04:57 +0000

    Add constructor and destructor to Application class

---
+++ lib/Application.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <limits>
+#include "Application.hpp"
+
+// Constructor
+Application::Application() {
+    initialiseMenus();
+}
+
+// Destructor
+Application::~Application() {}
