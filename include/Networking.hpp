44aac341ff71fe9e99a8f87f03c288241fbb3683
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-22 12:52:21 +0000

    Add Networking header file with necessary includes

---
+++ include/Networking.hpp
@@ -0,0 +1,15 @@
+#ifndef NETWORKING_HPP
+#define NETWORKING_HPP
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <memory>
+#include <string>
+#include <netinet/in.h>
+#include "Application.hpp"
+#include "Library.hpp"
+
+
+
+#endif // NETWORKING_HPP
