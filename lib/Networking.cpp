3c5d384c41322361e6ecad67c11b6f2218db9d2a
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-22 16:10:27 +0000

    Change server binding address to INADDR_ANY for improved accessibility

--- lib/Networking.cpp
+++ lib/Networking.cpp
@@ -29,7 +29,7 @@ Server::Server(int port, std::shared_ptr<LibraryManager> library_manager)
     }
 
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
