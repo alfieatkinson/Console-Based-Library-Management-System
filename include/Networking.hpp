d4113801f679609cf0cef17337b91941b2e1c68c
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-22 12:54:28 +0000

    Add Server class to handle client connections in Networking module

--- include/Networking.hpp
+++ include/Networking.hpp
@@ -10,6 +10,21 @@
 #include "Application.hpp"
 #include "Library.hpp"
 
+class Server {
+private:
+    int server_fd;
+    struct sockaddr_in address;
+    int addrlen;
+    std::shared_ptr<LibraryManager> library_manager;
+    std::vector<std::thread> client_threads;
 
+    void handleClient(int client_socket);
+
+public:
+    Server(int port, std::shared_ptr<LibraryManager> library_manager);
+    ~Server();
+
+    void start();
+};
 
 #endif // NETWORKING_HPP
