554b2b30295d60fa31419626ce58e9b08de503b2
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-22 13:00:37 +0000

    Implement Server constructor for handling socket setup and client connections

--- lib/Networking.cpp
+++ lib/Networking.cpp
@@ -9,3 +9,32 @@
 #include "Networking.hpp"
 #include "Application.hpp"
 #include "Library.hpp"
+
+Server::Server(int port, std::shared_ptr<LibraryManager> library_manager)
+    : library_manager(library_manager), addrlen(sizeof(address)) {
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+}
