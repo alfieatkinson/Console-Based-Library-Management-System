8c714e28cfca17df825182d4ef08563856131359
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-23 09:14:13 +0000

    Add includes for persistence and database functionality in PersistenceTest

--- tests/PersistenceTest.cpp
+++ tests/PersistenceTest.cpp
@@ -1,2 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <memory>
+
+#include "Persistence.hpp"
+#include "Database.hpp"
+#include "Book.hpp"
+#include "User.hpp"
+#include "Transaction.hpp"
