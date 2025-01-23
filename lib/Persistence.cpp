317ffc3e46e7c75f9eb8a9a9c11967e690d52d27
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-23 12:17:51 +0000

    Remove redundant string conversion function and include utility header in Persistence.cpp

--- lib/Persistence.cpp
+++ lib/Persistence.cpp
@@ -1,13 +1,7 @@
 #include "Persistence.hpp"
+#include "Utils.hpp"
 #include <fstream>
 #include <iostream>
-
-// Helper function to convert a string to lower case
-std::string toLowerCase(const std::string& input) {
-    std::string result = input;
-    std::transform(result.begin(), result.end(), result.begin(), ::tolower);
-    return result;
-}
 
 // Constructor
 PersistenceManager::PersistenceManager(const std::string& filename) : filename(filename) {}
