9fc692787652c5977d5b9e7c363255b70267823d
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-23 09:52:44 +0000

    Add background save functionality to ThreadManager

--- lib/Multithreading.cpp
+++ lib/Multithreading.cpp
@@ -18,3 +18,22 @@ void ThreadManager::periodicSaveTask(std::function<void()> saveFunction, std::ch
         }
     }
 }
+
+// Methods for background save
+void ThreadManager::startBackgroundSave(std::function<void()> saveFunction, std::chrono::minutes interval) {
+    if (running) {
+        return;
+    }
+    running = true;
+    background_save_thread = std::thread(&ThreadManager::periodicSaveTask, this, saveFunction, interval);
+}
+
+void ThreadManager::stopBackgroundSave() {
+    if (!running) {
+        return;
+    }
+    running = false;
+    if (background_save_thread.joinable()) {
+        background_save_thread.join();
+    }
+}
