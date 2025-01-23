fd11a8f9c77647a3ebbc06a34202ae1903b7d25c
alfieatkinson <alf.atkinson13@gmail.com>
2025-01-23 09:52:11 +0000

    Add periodicSaveTask method to ThreadManager for background data saving

--- lib/Multithreading.cpp
+++ lib/Multithreading.cpp
@@ -8,3 +8,13 @@ ThreadManager::~ThreadManager() {
     stopBackgroundSave();
     joinClientThreads();
 }
+
+// Method to periodically save data in the background
+void ThreadManager::periodicSaveTask(std::function<void()> saveFunction, std::chrono::minutes interval) {
+    while (running) {
+        std::this_thread::sleep_for(interval);
+        if (running) {
+            saveFunction();
+        }
+    }
+}
